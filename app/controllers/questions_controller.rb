9cba906a3f7f680a57d587ffc0482f55e66a81ff
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2022-11-14 15:41:33 -0300

    finished after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,9 +8,9 @@ class QuestionsController < ApplicationController
   end
 
   def coach_answer(question)
-    if question.downcase == "I am going to work"
+    if @question.capitalize == "I am going to work"
       'Great!'
-    elsif question.end_with?("?")
+    elsif @question.end_with?("?")
       "Silly question, get dressed and go to work!"
     else
       "I don't care, get dressed and go to work!"
