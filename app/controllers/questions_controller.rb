b55145875bbd3fbd701ef473fa7e6dc6d39a55c4
fernanda yamamoto fee.yamamoto@gmail.com <fee.yamamoto@gmail.com>
2023-01-02 08:04:36 -0300

    git test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
     if @question.capitalize == "I am going to work"
       'Great!'
     elsif @question.end_with?("?")
-      "Silly question, get dressed and go to work!"
+      "Silly question stupid!, get dressed and go to work!"
     else
       "I don't care, get dressed and go to work!"
     end
